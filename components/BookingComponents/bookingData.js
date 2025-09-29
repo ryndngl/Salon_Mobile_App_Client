b203335f1d98c1e234fc40df3239ccc5cfdbbd2d
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-30 00:01:08 +0800

    Add bookingData constants file for booking form

---
+++ components/BookingComponents/bookingData.js
@@ -0,0 +1,127 @@
+// Time slots available for booking
+export const timeSlots = [
+  "9:00 AM",
+  "9:30 AM",
+  "10:00 AM",
+  "10:30 AM",
+  "11:00 AM",
+  "11:30 AM",
+  "12:00 PM",
+  "12:30 PM",
+  "1:00 PM",
+  "1:30 PM",
+  "2:00 PM",
+  "2:30 PM",
+  "3:00 PM",
+  "3:30 PM",
+  "4:00 PM",
+  "4:30 PM",
+  "5:00 PM",
+];
+
+// All available services
+export const allServices = [
+  "Hair Cut",
+  "Hair Color",
+  "Rebond & Forms",
+  "Hair Treatment",
+  "Foot Spa",
+  "Nail Care",
+];
+
+// Hair Cut options and styles
+export const hairCutOptions = ["Men", "Women", "Kids"];
+
+export const haircutStyles = {
+  Men: [
+    "Buzz Cut",
+    "High Fade",
+    "Crew Cut",
+    "Curtain Fringe",
+    "Fohawk",
+    "French Crop",
+    "Side Part",
+    "Taper Fade Mohawk",
+    "Textured-Comb-Over",
+    "Two Block",
+    "Army Cut",
+    "Burst Fade",
+  ],
+  Women: [
+    "Boy Cut",
+    "Butterfly Cut",
+    "Fluffy Waves Bob",
+    "Layered Curls",
+    "Layered",
+    "Long Layered",
+    "Middy",
+    "Short",
+    "Soft and Pixie Cut",
+    "Textured Bob",
+    "V Cut",
+    "Wolf Cut Mallet",
+    "Wolf Cut",
+    "Bangs",
+    "Side swept",
+    "Sombre",
+  ],
+  Kids: [
+    "Little Army Cut",  
+    "Bowl Cut", 
+    "Little Buzz",      
+    "Little Comb Over Cut",
+    "Pompadour",
+    "Little Fade",      
+    "Fringe Fade",
+    "Little High Fade",
+    "Mid Fade",
+    "Mohawk",
+    "Side Part Cut",
+  ],
+};
+
+// Hair Color options and styles
+export const hairColorOptions = [
+  "Root Touch Up",
+  "Full Hair",
+  "Highlights",
+  "Balayage",
+];
+
+export const hairColorStyles = {
+  "Root Touch Up": [
+    "Medium Brown",
+    "Light Blonde",
+    "Black",
+    "Auburn Tones",
+    "Dark Brown",
+  ],
+  "Full Hair": [
+    "Inky Grey",
+    "Blonde",
+    "Purple",
+    "Chestnut Brown",
+    "Plum",
+    "Light Golden Brown",
+    "Ember",
+  ],
+  Highlights: [
+    "Money Piece",
+    "Copper",
+    "Blue",
+    "Cherry Red",
+    "Honey Blonde",
+    "Ombre",
+    "Toasted Caramel",
+    "Chunky",
+  ],
+  Balayage: [
+    "Ash Blonde",
+    "Burgundy",
+    "Auburn",
+    "Bronde",
+    "Chocolate Brown",
+    "Silver",
+    "Golden Caramel",
+  ],
+};
