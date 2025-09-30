58dc1c0b5365a59c24e590d4c5797edfc7c7f0f6
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-30 21:57:48 +0800

    Export ProfileScreen hooks from hooks index

--- hooks/index.js
+++ hooks/index.js
@@ -1,15 +1,29 @@
-// hooks/index.js
+
+// Core/API hooks
 export { useApi } from './useApi';
+
+// HomeScreen hooks
 export { useHomeScreen } from './useHomeScreen';
 export { useTestimonials } from './useTestimonials';
+
+// AuthComponents hooks
 export { useLoginAnimation } from './useLoginAnimation';
 export { useLoginForm } from './useLoginForm';
 export { useRegisterForm } from './useRegisterForm';
-export { useForgotPasswordAnimation } from './useForgotPasswordAnimation';  
-export { useTokenValidation } from './useTokenValidation';                  
-export { useForgotPassword } from './useForgotPassword';               
+export { useForgotPasswordAnimation } from './useForgotPasswordAnimation';
+export { useTokenValidation } from './useTokenValidation';
+export { useForgotPassword } from './useForgotPassword';
 export { useResetPasswordAnimation } from './useResetPasswordAnimation';
 export { useResetPassword } from './useResetPassword';
+
+// BookingComponents hooks
 export { useBookingForm } from './useBookingForm';
 export { useBookingManagement } from './useBookingManagement';
+
+// ServiceComponents hooks
 export { useServiceDetail } from './useServiceDetail';
+
+// ProfileComponents hooks
+export { useProfileData } from './useProfileData';
+export { usePhoneEditor } from './usePhoneEditor';
+export { useLogoutFlow } from './useLogoutFlow';
